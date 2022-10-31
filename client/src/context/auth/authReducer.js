a3526f5668071b76de804e9de321c84a717f9c6e
jenatayeva <atayevajennet1126@gmail.com>
2022-10-31 19:17:15 +0500

    done

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -9,7 +9,6 @@ import {
   CLEAR_ERRORS
 } from '../types' 
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default (state, action) => {
   switch(action.type){
     case USER_LOADED:
@@ -31,16 +30,6 @@ export default (state, action) => {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
-    case LOGOUT:
-      localStorage.removeItem('token')
-      return {
-        ...state,
-        token: null,
-        isAuthenticated: false,
-        loading: false,
-        user: null,
-        error: action.payload
-      }
     case LOGOUT:
       localStorage.removeItem('token')
       return {
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -82,7 +82,7 @@ const ContactState = props => {
     }
     try {
       const res = await axios.put(`/api/contacts/${contact._id}`, contact, config)
-      dispatch({ type: UPDATE_CONTACT, payload: res.data })
+      dispatch({ type: UPDATE_CONTACT, payload: contact })
 
     } catch (error) {
       dispatch({
